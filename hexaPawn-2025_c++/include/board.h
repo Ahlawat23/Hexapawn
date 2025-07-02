15232ba6b061cda4404ae02e61e24ec35d300f3b
Reaper23 <pankajhlwt@gmail.com>
2025-07-02 23:58:48 +0530

    created board

--- hexaPawn-2025_c++/include/board.h
+++ hexaPawn-2025_c++/include/board.h
@@ -1,12 +1,23 @@
 #pragma once
-//this gonna handle the drawing part
+
+#include <SDL3/SDL.h>
+
+enum class SquareColour{
+    Darker,
+    Lighter
+};
+
+
+// This handles the drawing part
 class Board
 {
-    private: 
-        int data;
-    public:
+    private:
         Board();
-        void Draw();
+        void DrawSquare(SDL_Renderer* renderer, SquareColour col, int x, int y );
+
+    public:
+        static Board& instance(); 
+
+        void Draw(SDL_Renderer* renderer);
         void onceDragIsDonePlayTheEnemy();
-        
 };
--- hexaPawn-2025_c++/src/board.cpp
+++ hexaPawn-2025_c++/src/board.cpp
@@ -1,16 +1,50 @@
-#include<board.h>
+#include <board.h>
 
-using namespace std;
 
-Board::Board(){
+Board::Board() {
+    // Initialization code
+}
+
+Board& Board::instance() {
+    static Board singleton_instance;
+    return singleton_instance;
+}
+
+/* This function runs once per frame, and it contains all the draw logic */
+void Board::Draw(SDL_Renderer* renderer) {
+
+    SDL_SetRenderDrawColorFloat(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+    SDL_RenderClear(renderer);
+
+    DrawSquare(renderer, SquareColour::Darker, 0, 0);
     
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(renderer);
 }
 
-void Board::Draw(){
-  
+void Board::DrawSquare(SDL_Renderer* renderer, SquareColour col, int x, int y ){
+
+    SDL_FRect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = 300;
+    rect.h = 300;
+
+    switch (col) {
+            break;
+        case SquareColour::Darker:
+            SDL_SetRenderDrawColor(renderer, 2, 86, 157, SDL_ALPHA_OPAQUE); // blue
+            break;
+        case SquareColour::Lighter:
+            SDL_SetRenderDrawColor(renderer, 3, 129, 207, SDL_ALPHA_OPAQUE); // light blue
+            break;
+    }
+    SDL_RenderFillRect(renderer, &rect);
+
 }
 
-void Board::onceDragIsDonePlayTheEnemy(){
-   
+void Board::onceDragIsDonePlayTheEnemy() {
+    // Enemy move code
 }
 
